01229e0af55b459d502e37a0ed96b5cfe6344333
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-04-21 21:07:29 +0200

    Include components in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
