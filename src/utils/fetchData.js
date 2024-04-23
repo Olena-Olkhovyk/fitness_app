73b4fe3948533b6aef0906ef967843f57b5fe0d7
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-04-23 10:19:17 +0200

    Env

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   params: { limit: "10" },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    "X-RapidAPI-Key": "9647a49227msh987541702074902p1ae020jsn7d3ce606d08f",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
