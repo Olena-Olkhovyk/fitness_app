66c6aa90ac2f91a36fbd1214b712bf549934d93f
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-04-24 09:46:59 +0200

    Display similar target and equipment videos

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,7 +1,55 @@
 import React from "react";
-
-const SimilarExercises = () => {
-  return <div>SimilarExercises</div>;
+import { Box, Stack, Typography } from "@mui/material";
+import HorizontalScrollbar from "././HorizontalScrollBar";
+import Loader from "./Loader";
+const SimilarExercises = ({ equipmentMuscleExer, targetMuscleExer }) => {
+  console.log("tar", targetMuscleExer);
+  return (
+    <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
+      <Typography
+        sx={{ fontSize: { lg: "44px", xs: "25px" }, ml: "20px" }}
+        fontWeight={700}
+        color="#000"
+        mb="33px"
+      >
+        Similar{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          Target Muscle
+        </span>{" "}
+        exercises
+      </Typography>
+      <Stack direction="row" sx={{ p: 2, position: "relative" }}>
+        {targetMuscleExer.length !== 0 ? (
+          <HorizontalScrollbar data={targetMuscleExer} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography
+        sx={{
+          fontSize: { lg: "44px", xs: "25px" },
+          ml: "20px",
+          mt: { lg: "100px", xs: "60px" },
+        }}
+        fontWeight={700}
+        color="#000"
+        mb="33px"
+      >
+        Similar{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          Equipment
+        </span>{" "}
+        exercises
+      </Typography>
+      <Stack direction="row" sx={{ p: 2, position: "relative" }}>
+        {equipmentMuscleExer.length !== 0 ? (
+          <HorizontalScrollbar data={equipmentMuscleExer} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default SimilarExercises;
