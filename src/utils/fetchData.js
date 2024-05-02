830f382a92a13176b5053b8f27eaba7091dbcafa
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-05-02 16:16:38 +0200

    Change key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   params: { limit: "10" },
   headers: {
-    "X-RapidAPI-Key": "9647a49227msh987541702074902p1ae020jsn7d3ce606d08f",
+    "X-RapidAPI-Key": "e26c7aa98emshe7802a94b9adc69p1f497cjsn522c46a951d0",
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
   },
 };
@@ -11,7 +11,7 @@ export const youtubeOptions = {
   method: "GET",
 
   headers: {
-    "X-RapidAPI-Key": "9647a49227msh987541702074902p1ae020jsn7d3ce606d08f",
+    "X-RapidAPI-Key": "e26c7aa98emshe7802a94b9adc69p1f497cjsn522c46a951d0",
     "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
   },
 };
